524c4247c8ae1d50f64fd9ae8b64991712eea48c
chaowang2333 <heartwc3@gmail.com>
2019-02-20 10:54:14 +1100

    catslave

---
+++ Base/Map/MapViewController.swift
@@ -0,0 +1,45 @@
+//
+//  MapViewController.swift
+//  CatSlave
+//
+//  Created by crow on 9/10/17.
+//  Copyright © 2017 crow. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        // pick photo
+//        if (FIRAuth.auth()?.currentUser) == nil
+//        {
+//            //login
+//            let vc = storyboard?.instantiateViewController(withIdentifier: "LoginViewController")
+//            self.present(vc, animated: true, completion: nil)
+//        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
